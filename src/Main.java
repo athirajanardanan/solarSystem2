75874f706cfd465dfca52a43e5a8d55cb243f1ea
athirajanardanan <athi.j89@gmail.com>
2022-12-18 12:25:50 -0500

    The basics of solar Ssystem

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        solarSystem XX=new solarSystem();
+        XX.moon=1;
+        XX.sun=1;
+        XX.Planet=2;
+    }
+}
